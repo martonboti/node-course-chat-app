64ae6ff6077f89fb26f13f858b0dd066402b3529
unknown <martonbotix@gmail.com>
2018-04-27 12:23:47 +0300

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname , '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'Andrew',
+    text: 'How is it goin',
+    createdAt: 1234543
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('CreateMessage', newMessage);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  })
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
